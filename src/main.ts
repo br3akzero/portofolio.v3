35cd2b3c9362a0f7e6e6f8a0a6e7f36ffd50bbd8
Rashadatjou <rashadatjou.mehdi@gmail.com>
2025-05-01 15:34:31 +0000

    feat(*): initial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,1 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML += `<p>Generated</p>`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
